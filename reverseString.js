0e9a3014251daead2e781994e8f7e8d9c834d98e
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 03:43:29 -0500

    moving to the next alogritim

--- reverseString.js
+++ reverseString.js
@@ -31,3 +31,36 @@ function reverseString(str){
 
 // excuting the function
 reverseString('treek');
+
+
+function reverseString2(str){
+    //step one create an empty string that will host a new string
+    var newString ="";
+    
+    // create the for loop
+    // the starting point of the loop will be (str.length-1) which corresponds to the
+    // last character of the string "o"
+    // as long as i is greater than or equals 0, the loop will go on
+    // We decrement i after each iteration */
+    
+    for(var i = str.length -1; i > 0 ; i--){
+    newString += str[i]; // or newString = newString + str[i];
+    
+    /* here is the background
+    For each iteration: i = str.length -1 and newString = newString + str[i]
+      First iteration: i  =(5-1) = 4  newString ="" + "o"
+      Second iteration: i =(4-1) = 3 newString ="o"+"l"
+      third irteration : i = (3-1) = 2 newString ="ol" +"l"
+      two iteration: i =(2-1) = 1 newString ="oll" + "e";
+      one iteration: i = (1 -1) =0 newString= "olle" "h";
+      for example
+    */
+      return newString ; // reverses the word
+    
+    }
+    
+    }
+         reverseString2("keepMovingForward");
+
+    
+    
--- reverseString2.js
+++ reverseString2.js
@@ -4,28 +4,36 @@ function reverseString(str){
 //step one create an empty string that will host a new string
 var newString ="";
 
+console.log(str);
 // create the for loop
 // the starting point of the loop will be (str.length-1) which corresponds to the
 // last character of the string "o"
 // as long as i is greater than or equals 0, the loop will go on
 // We decrement i after each iteration */
 
-for(var i = str.length -1; i > 0 ; i--){
-newString += str[i]; // or newString = newString + str[i];
+console.log(str.length);
 
-/* here is the background
-For each iteration: i = str.length -1 and newString = newString + str[i]
-  First iteration: i  =(5-1) = 4  newString ="" + "o"
-  Second iteration: i =(4-1) = 3 newString ="o"+"l"
-  third irteration : i = (3-1) = 2 newString ="ol" +"l"
-  two iteration: i =(2-1) = 1 newString ="oll" + "e";
-  one iteration: i = (1 -1) =0 newString= "olle" "h";
-  for example
-*/
-  return newString ;
+for (var i = str.length - 1; i >= 0; i--) { 
+    newString += str[i]; // or newString = newString + str[i];
+
+/* Here hello's length equals 5
+    For each iteration: i = str.length - 1 and newString = newString + str[i]
+    First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+    Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+    Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+    Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+    Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh"
+End of the FOR Loop*/
+
+// Step 3. Return the reversed string
+
 
 }
+console.log(newString);
+return newString;
+
 
 }
+     reverseString("whale");
 
-// 
+
