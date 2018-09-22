48c5780e111f0b5c054ee0f938c6d9d67f968990
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 17:12:42 -0500

    keep solving problems

--- longestWordFinder.js
+++ longestWordFinder.js
@@ -5,10 +5,17 @@ function findLongestWord(str){
     var splitWord = str.split(" ");
 
     // Intiaite a variable that hold the length of the world
-    var longestword = 0;
+    var longestWord = 0;
 
     // Step 3. Create a for loop
     for(var i = 0; i < strSplit.length ; i++){
-        
+        if(splitWord[i].length > longestWord){
+        // if the word is longer than the longest word
+        // reassign the longest word to it {
+            longestWord = splitWord[i].length; // longestWord has been assigned the  the true condition statement or the longest i world
+        }
+        return longestWord;
     }
 }
+
+findLongestWord("this should be an working example treek");
