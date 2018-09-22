b4ed5e54ebb802c5dd8ddd16c23d958e0a3dc1b7
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 02:48:43 -0500

    another version on how to reverse a string

--- reverseString2.js
+++ reverseString2.js
@@ -4,6 +4,12 @@ function reverseString(str){
 //step one create an empty string that will host a new string
 var newString ="";
 
+// create the for loop
+// the starting point of the loop will be (str.length-1) which corresponds to the
+// last character of the string "o"
+// as long as i is greater than or equals 0, the loop will go on
+// We decrement i after each iteration */
+
 }
 
 // 
