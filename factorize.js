4ebe1c7dd0827c71352dd91f10a4e886ec3f1b2b
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 14:22:30 -0500

    learn how to factoize algorithm

--- factorize.js
+++ factorize.js
@@ -2,4 +2,11 @@
 
 function factorize() {
     // if num == 0 or num == 1. it will return 
+    if(num === 0 || num === 1){
+        return 1;
+    }
+    // start the for loop with i == 4
+    // then you count backwards from there
+
+
 }
