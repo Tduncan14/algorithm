ca709f80e0eebc18ea2d32a63bbe0a16e5f4c96f
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 16:19:57 -0500

    keep solving problems

---
+++ longestWordFinder.js
@@ -0,0 +1,1 @@
+// Finding the longest word with a for loop
--- whileFactorize.js
+++ whileFactorize.js
@@ -1,1 +1,31 @@
 // factorize with a while loop
+
+function factor(num){
+    // create a variable result to store num;
+    var result = num;
+
+    // if num = 0 or num = 1 , the factorial will return 1
+    if(num === 0 || num === 1) {
+        return 1;
+    }
+    // now it is time to create the while loop
+    while(num > 1){
+        num --; // decremenation by 1 each iteration or each loop
+        result = result *num ; // also result *= num works too
+
+        // 1st loop = 5
+        // 2nd loop = 5 *4
+        // 3 loop = 5*4*3
+        // 4th loop = 5*4 *3 * 2
+        // 5th loop = 5 *4*3*2*1
+        // the while loops ends and you should get the number you looking for
+
+
+
+    }
+    console.log(result);
+     return result;
+}
+
+
+factor(5);
