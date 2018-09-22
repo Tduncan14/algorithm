a12793b95670d71e2bcc307a2336bab09ea57992
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 16:23:00 -0500

    keep solving problems

--- longestWordFinder.js
+++ longestWordFinder.js
@@ -1,1 +1,8 @@
 // Finding the longest word with a for loop
+
+function findLongestWord(str){
+    // split the word into array
+    var splitWord = str.split(" ");
+
+    // Intiaite a variable that hold the length of the world
+}
