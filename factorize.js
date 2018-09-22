29486304a647b6b94fa9332a8fecf1e088d882ee
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 14:30:04 -0500

    keep coding and moving forward

--- factorize.js
+++ factorize.js
@@ -1,17 +1,25 @@
 //factorizing a number function
 
-function factorize() {
+function factorize(num) {
     // if num == 0 or num == 1. it will return 
     if(num === 0 || num === 1){
+        console.log(1);
         return 1;
     }
     // start the for loop with i == 4
     // then you count backwards from there
 
     for(var i = num - 1; i > 1; i--){
-        
+
+        num = num * i // also num *=  i is just fine for a solution as well
+
     }
 
-
+    console.log(num);
+  return num
 
 }
+
+ factorize(0);
+ 
+ 
