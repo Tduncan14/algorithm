a8c237a6128a43f20ef93e71046458753fe75f26
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 02:54:43 -0500

    another version on how to reverse a string

--- reverseString2.js
+++ reverseString2.js
@@ -10,6 +10,15 @@ var newString ="";
 // as long as i is greater than or equals 0, the loop will go on
 // We decrement i after each iteration */
 
+for(var i = str.length -1; i > 0 ; i--){
+newString += str[i]; // or newString = newString + str[i];
+
+/* here is the background
+
+*/
+
+}
+
 }
 
 // 
