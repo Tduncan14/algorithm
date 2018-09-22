a21aa2eb2058acd41a16ca779a395faffbb80a29
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 00:42:13 -0500

    this an algortim for reverse string

--- reverseString.js
+++ reverseString.js
@@ -1,8 +1,16 @@
 // reverse the string algorithim
 
 function reverseString(){
+    // splits the string up and pushes it into a array
      var splitString = str.split();
 
      console.log(splitString);
 
+      // attaches the reverses method to splitString// that reverse the string letters
+     var reverseString = splitString.reverse();
+     
+
 }
+
+// excuting the function
+reverseString('treek');
