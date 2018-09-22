15bd8272750667e732e2a287460f629d8f427719
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-22 03:17:15 -0500

    another version on how to reverse a string

--- reverseString2.js
+++ reverseString2.js
@@ -18,9 +18,11 @@ For each iteration: i = str.length -1 and newString = newString + str[i]
   First iteration: i  =(5-1) = 4  newString ="" + "o"
   Second iteration: i =(4-1) = 3 newString ="o"+"l"
   third irteration : i = (3-1) = 2 newString ="ol" +"l"
-  two iteration: i =(2 -1) = 1 newString ="oll" + "e";
-  one iteration: i = ()
+  two iteration: i =(2-1) = 1 newString ="oll" + "e";
+  one iteration: i = (1 -1) =0 newString= "olle" "h";
+  for example
 */
+  return newString ;
 
 }
 
