6a60a08ae7a601965033065a9cf3454fc0af212c
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-25 20:13:52 -0500

    practicing my algorithms to 9

--- FindingLargestNinArray.js
+++ FindingLargestNinArray.js
@@ -1,1 +1,10 @@
-//
+//  Finding the  largest number in the arrays
+
+function largestOfFour(arr) {
+
+    // Step 1. Create an array that will host the result of the 4 sub arrays
+
+    var largestNumbers = [0,0,0,0];
+
+    // Step 2. Create the first For loop that will iterate through the arrays
+}
