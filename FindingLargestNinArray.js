d13c0896654728e85e9d3d3ccdf203dfa775b2b5
Tre'Shawn Duncan <nbatre1@yahoo.com>
2018-09-25 20:53:08 -0500

    this is a great feeling

--- FindingLargestNinArray.js
+++ FindingLargestNinArray.js
@@ -1,29 +1,33 @@
 //  Finding the  largest number in the arrays
 
+
+
 function largestOfFour(arr) {
 
     // Step 1. Create an array that will host the result of the 4 sub arrays
-
     var largestNumbers = [0,0,0,0];
+
 
     // Step 2. Create the first For loop that will iterate through the arrays
     for(var i = 0; i < arr.length; i++){
         /* The starting point, index 0, corresponds to the first sub- array */
     
+        console.log("is the number getting to here");
     // Step 3. Create the second For loop that will iterate through the sub -array
-     for(var j = 0 ; j < arr[i].length; i++) {
+     for(var j = 0; j < arr[i].length; j++) {
          /* the starting point, index 0 , corresponds to the first sub -array */
           
          if(arr[i][j] > largestNumbers[i]){
            // reassigning the largestNumber value to array[i][j] value
-            
-           largestNumbers[arrayIndex] = arr[i][j];
+            console.log()
+           largestNumbers[i] = arr[i][j];
          }
 
         }
     }
+    console.log(largestNumbers);
     return largestNumbers
 }
 
-  largestNumbers[[44,45,78,9],[22,89,90,15],[80,28,39,45],[1,2,4,5]];
+  largestOfFour([[44,45,78,9],[22,89,90,15],[80,28,39,45],[1,2,4,5]]);
   
